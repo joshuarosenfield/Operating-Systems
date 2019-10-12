699c3173d4724cd85595355b82177fdd840ab86e
joshuarosenfield <47152399+joshuarosenfield@users.noreply.github.com>
2019-10-12 19:49:05 -0400

    updated working testfile

--- proj2/part3/testfile.c
+++ proj2/part3/testfile.c
@@ -1,11 +1,56 @@
-#include<stdio.h>
-#include<syscall.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 #define __NR_START_ELEVATOR 436
 #define __NR_ISSUE_REQUEST 437
 #define __NR_STOP_ELEVATOR 438
+
+int start_elevator(){
+	return syscall(__NR_START_ELEVATOR);
+}
+
+int issue_request(int passenger_type, int start_floor, int destination_floor){
+	return syscall(__NR_ISSUE_REQUEST, passenger_type, start_floor, destination_floor);
+}
+
+int stop_elevator(void){
+        return syscall(__NR_STOP_ELEVATOR);
+}
+
 int main(){
-	printf("test start: %d\n", syscall(__NR_START_ELEVATOR);
-	printf("test add: %d\n", syscall(__NR_ISSUE_REQUEST,1,1,2);
-	printf("test stop: %d\n", syscall(__NR_STOP_ELEVATOR);
+
+	long ret;
+
+	ret  = start_elevator();
+
+	if (ret < 0)
+		perror("system call error");
+	else
+		printf("Function successful. returned %ld\n", ret);
+
+	printf("Returned value: %ld\n", ret);
+
+
+        ret  = issue_request(1,1,1);
+
+        if (ret < 0)
+                perror("system call error");
+        else
+                printf("Function successful. returned %ld\n", ret);
+
+        printf("Returned value: %ld\n", ret);
+
+
+	ret  = stop_elevator();
+
+        if (ret < 0)
+                perror("system call error");
+        else
+                printf("Function successful. returned %ld\n", ret);
+
+        printf("Returned value: %ld\n", ret);
+
 	return 0;
 }
